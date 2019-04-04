04a95da8288fc451a3f4271bca10fc0c9faf9eb0
blelodet2018 <blelodet2018@27-UC37-05.ad.campus-eni.fr>
2019-04-04 09:32:52 +0200

    select enchere la plus haute pour un article

--- ENI-Encheres/src/fr/eni/enchere/dal/ArticleVenduDAO.java
+++ ENI-Encheres/src/fr/eni/enchere/dal/ArticleVenduDAO.java
@@ -3,6 +3,8 @@ package fr.eni.enchere.dal;
 import java.util.List;
 
 import fr.eni.enchere.bo.ArticleVendu;
+import fr.eni.enchere.bo.Categorie;
+import fr.eni.enchere.bo.Utilisateur;
 import fr.eni.enchere.BusinessException;
 
 public interface ArticleVenduDAO {
@@ -11,5 +13,7 @@ public interface ArticleVenduDAO {
 	public void delete(ArticleVendu article) throws BusinessException;
 	public ArticleVendu select(int id) throws BusinessException;
 	public List<ArticleVendu> lister() throws BusinessException;
+	public List<ArticleVendu> selectByCategorie(Categorie categ) throws BusinessException ;
+	public List<ArticleVendu> selectByVendeur(Utilisateur usr) throws BusinessException ;
 
 }
