8e9250edcff4a70e5ce0aa996e674d1c3ec2de5a
qbaudry2018 <qbaudry2018@27-UC37-21.ad.campus-eni.fr>
2019-04-05 16:19:01 +0200

    redirection article

--- ENI-Encheres/WebContent/WEB-INF/pages/ajax/ajax_listEnchere.jsp
+++ ENI-Encheres/WebContent/WEB-INF/pages/ajax/ajax_listEnchere.jsp
@@ -16,10 +16,10 @@
 					</div>
 					<div class="col-md-8">
 						<div class="card-body">
-							<form method="post" id="art${article.no_article}" action="/detailEnchere">
+							<form method="post" id="art${article.no_article}" action="${pageContext.request.contextPath}/detailEnchere">
 								<input type="hidden" name="no_article" value="${article.no_article}">
 							</form>
-							<h5 class="card-title"><a onclick="$('#art${article.no_article}')" href="">${article.nom_article}</a></h5>
+							<h5 class="card-title"><a onclick="$('#art${article.no_article}').submit();">${article.nom_article}</a></h5>
 							<c:choose>
 								<c:when test="${article.concerne != null}">
 									<p class="card-text">Prix : ${article.concerne.montant_enchere} points
--- ENI-Encheres/src/fr/eni/enchere/ihm/detailEnchere.java
+++ ENI-Encheres/src/fr/eni/enchere/ihm/detailEnchere.java
@@ -1,7 +1,10 @@
 package fr.eni.enchere.ihm;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class detailEnchere
  */
+@WebServlet("/detailEnchere")
 public class detailEnchere extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -34,6 +38,9 @@ public class detailEnchere extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String art = request.getParameter("no_article");
 		System.out.println(art);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/pages/DetailsArticle.jsp");
+		rd.forward(request, response);
 	}
 
 }
