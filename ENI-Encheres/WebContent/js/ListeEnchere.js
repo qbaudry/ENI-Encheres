4d275d05b2088379857be4fca240efc311eb9ebc
blelodet2018 <blelodet2018@27-UC37-05.ad.campus-eni.fr>
2019-04-05 11:00:33 +0200

    js

--- ENI-Encheres/WebContent/js/ListeEnchere.js
+++ ENI-Encheres/WebContent/js/ListeEnchere.js
@@ -5,6 +5,23 @@ $( document ).ready(function() {
 	$('#filtre').on('keyup paste',function(){
 		filtrer();		
 	});
+	filtrer();
+	$('#achat').click(function(){
+		$('enchere_ouverte').prop('disabled','');
+		$('enchere_encours').prop('disabled','');
+		$('enchere_remportees').prop('disabled','');
+		$('ventes_encours').prop('disabled','disabled');
+		$('ventes_non_debutees').prop('disabled','disabled');
+		$('ventes_terminees').prop('disabled','disabled');
+	});
+	$('#achat').click(function(){
+		$('enchere_ouverte').prop('disabled','disabled');
+		$('enchere_encours').prop('disabled','disabled');
+		$('enchere_remportees').prop('disabled','disabled');
+		$('ventes_encours').prop('disabled','');
+		$('ventes_non_debutees').prop('disabled','');
+		$('ventes_terminees').prop('disabled','');
+	});
 });
 
 function filtrer(){
